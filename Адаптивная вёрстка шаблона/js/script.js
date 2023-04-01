e17512f1949de5e4a12473d40acd001f5bda639c
GorbunovArtem <106732889+GorbunovArtem@users.noreply.github.com>
2023-04-01 10:20:20 +0500

    Add files via upload

---
+++ Адаптивная вёрстка шаблона/js/script.js
@@ -0,0 +1,6 @@
+function buttonFocus() {
+  document.querySelectorAll("button").color = rgb(146, 50, 103);
+  document.querySelectorAll("button").background.color = white;
+  document.querySelectorAll("button").color = white;
+}
+function buttonUnFocus() {}
